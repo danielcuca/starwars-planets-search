507fb94f98a8ec34622f6d8589acebd36a520aa1
danielcuca <elzinhocuca@yahoo.com>
2023-05-31 18:11:03 -0300

    3 mediano

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -37,6 +37,29 @@ export default function Table() {
         value={ filtered }
         data-testid="name-filter"
       />
+      <form>
+        <select data-testid="column-filter">
+          <option value="population">population</option>
+          <option value="orbital_period">orbital_period</option>
+          <option value="diameter">diameter</option>
+          <option value="rotation_period">rotation_period</option>
+          <option value="surface_water">surface_water</option>
+        </select>
+
+        <select data-testid="comparison-filter">
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+
+        <input
+          data-testid="value-filter"
+          type="number"
+        />
+
+        <button data-testid="button-filter">FILTRAR</button>
+
+      </form>
       <table>
         <thead>
           <tr>
